750875419edcbc240859f0a36c209c2e656739e2
lucasnwvalladares <lucasnwvalladares@gmail.com>
2019-11-18 13:57:24 -0300

    Find User updated

--- src/main/java/com/utface/apirest/repository/UserRepository.java
+++ src/main/java/com/utface/apirest/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package com.utface.apirest.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.utface.apirest.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	
 	User findById(long id);
-	List<User> findByRole(String userrole);
+	
 }
