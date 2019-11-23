ad7de386333fc2cde6905e76add1fdbc4be4147c
lucasnwvalladares <lucasnwvalladares@gmail.com>
2019-11-22 22:36:49 -0300

    application properties correction

--- src/main/java/com/utface/apirest/models/StudentClass.java
+++ src/main/java/com/utface/apirest/models/StudentClass.java
@@ -2,14 +2,11 @@ package com.utface.apirest.models;
 
 import java.io.Serializable;
 
-import java.util.List;
-
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
-import javax.persistence.OneToMany;
 
 import javax.persistence.Table;
 
